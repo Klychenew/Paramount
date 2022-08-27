346568be052bf34c9d668af05cc226a18e356406
Klychenew <calichenew@gmail.com>
2022-08-26 19:07:23 -0500

    subo estructura 1

---
+++ code.js
@@ -0,0 +1,7 @@
+let bodyPag = document.querySelector("body")
+let navbar = document.createElement ("nav")
+navbar.classList.add("navbar", "navbar-expand-lg", "bg-light")
+bodyPag.appendChild (navbar)
+
+let divHijoNavbar = document.createElement ("div")
+divHijoNavbar.className = "container-fluid"
